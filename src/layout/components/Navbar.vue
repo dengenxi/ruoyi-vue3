4f318e58fdd13a82e34bfa0f65eaa72f9461c070
dengenxi <coderqin@foxmail.com>
2025-02-28 15:42:21 +0800

    优化tags-view样式

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="navbar">
-    <hamburger id="hamburger-container" :is-active="appStore.sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
-    <breadcrumb id="breadcrumb-container" class="breadcrumb-container" v-if="!settingsStore.topNav" />
-    <top-nav id="topmenu-container" class="topmenu-container" v-if="settingsStore.topNav" />
+    <hamburger
+      id="hamburger-container"
+      :is-active="appStore.sidebar.opened"
+      class="hamburger-container"
+      @toggleClick="toggleSideBar"
+    />
+    <breadcrumb
+      id="breadcrumb-container"
+      class="breadcrumb-container"
+      v-if="!settingsStore.topNav"
+    />
+    <top-nav
+      id="topmenu-container"
+      class="topmenu-container"
+      v-if="settingsStore.topNav"
+    />
 
     <div class="right-menu">
       <template v-if="appStore.device !== 'mobile'">
@@ -19,7 +32,10 @@
         <screenfull id="screenfull" class="right-menu-item hover-effect" />
 
         <el-tooltip content="主题模式" effect="dark" placement="bottom">
-          <div class="right-menu-item hover-effect theme-switch-wrapper" @click="toggleTheme">
+          <div
+            class="right-menu-item hover-effect theme-switch-wrapper"
+            @click="toggleTheme"
+          >
             <svg-icon v-if="settingsStore.isDark" icon-class="sunny" />
             <svg-icon v-if="!settingsStore.isDark" icon-class="moon" />
           </div>
@@ -30,7 +46,11 @@
         </el-tooltip>
       </template>
       <div class="avatar-container">
-        <el-dropdown @command="handleCommand" class="right-menu-item hover-effect" trigger="click">
+        <el-dropdown
+          @command="handleCommand"
+          class="right-menu-item hover-effect"
+          trigger="click"
+        >
           <div class="avatar-wrapper">
             <img :src="userStore.avatar" class="user-avatar" />
             <el-icon><caret-bottom /></el-icon>
@@ -40,7 +60,10 @@
               <router-link to="/user/profile">
                 <el-dropdown-item>个人中心</el-dropdown-item>
               </router-link>
-              <el-dropdown-item command="setLayout" v-if="settingsStore.showSettings">
+              <el-dropdown-item
+                command="setLayout"
+                v-if="settingsStore.showSettings"
+              >
                 <span>布局设置</span>
               </el-dropdown-item>
               <el-dropdown-item divided command="logout">
@@ -55,25 +78,25 @@
 </template>
 
 <script setup>
-import { ElMessageBox } from 'element-plus'
-import Breadcrumb from '@/components/Breadcrumb'
-import TopNav from '@/components/TopNav'
-import Hamburger from '@/components/Hamburger'
-import Screenfull from '@/components/Screenfull'
-import SizeSelect from '@/components/SizeSelect'
-import HeaderSearch from '@/components/HeaderSearch'
-import RuoYiGit from '@/components/RuoYi/Git'
-import RuoYiDoc from '@/components/RuoYi/Doc'
-import useAppStore from '@/store/modules/app'
-import useUserStore from '@/store/modules/user'
-import useSettingsStore from '@/store/modules/settings'
+import { ElMessageBox } from "element-plus";
+import Breadcrumb from "@/components/Breadcrumb";
+import TopNav from "@/components/TopNav";
+import Hamburger from "@/components/Hamburger";
+import Screenfull from "@/components/Screenfull";
+import SizeSelect from "@/components/SizeSelect";
+import HeaderSearch from "@/components/HeaderSearch";
+import RuoYiGit from "@/components/RuoYi/Git";
+import RuoYiDoc from "@/components/RuoYi/Doc";
+import useAppStore from "@/store/modules/app";
+import useUserStore from "@/store/modules/user";
+import useSettingsStore from "@/store/modules/settings";
 
-const appStore = useAppStore()
-const userStore = useUserStore()
-const settingsStore = useSettingsStore()
+const appStore = useAppStore();
+const userStore = useUserStore();
+const settingsStore = useSettingsStore();
 
 function toggleSideBar() {
-  appStore.toggleSideBar()
+  appStore.toggleSideBar();
 }
 
 function handleCommand(command) {
@@ -90,28 +113,30 @@ function handleCommand(command) {
 }
 
 function logout() {
-  ElMessageBox.confirm('确定注销并退出系统吗？', '提示', {
-    confirmButtonText: '确定',
-    cancelButtonText: '取消',
-    type: 'warning'
-  }).then(() => {
-    userStore.logOut().then(() => {
-      location.href = '/index';
+  ElMessageBox.confirm("确定注销并退出系统吗？", "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
+    .then(() => {
+      userStore.logOut().then(() => {
+        location.href = "/index";
+      });
     })
-  }).catch(() => { });
+    .catch(() => {});
 }
 
-const emits = defineEmits(['setLayout'])
+const emits = defineEmits(["setLayout"]);
 function setLayout() {
-  emits('setLayout');
+  emits("setLayout");
 }
 
 function toggleTheme() {
-  settingsStore.toggleTheme()
+  settingsStore.toggleTheme();
 }
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .navbar {
   height: 50px;
   overflow: hidden;
@@ -179,7 +204,7 @@ function toggleTheme() {
 
         svg {
           transition: transform 0.3s;
-          
+
           &:hover {
             transform: scale(1.15);
           }
--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="tags-view-container" class="tags-view-container">
-    <scroll-pane ref="scrollPaneRef" class="tags-view-wrapper" @scroll="handleScroll">
+    <scroll-pane
+      ref="scrollPaneRef"
+      class="tags-view-wrapper"
+      @scroll="handleScroll"
+    >
       <router-link
         v-for="tag in visitedViews"
         :key="tag.path"
@@ -14,39 +18,52 @@
       >
         {{ tag.title }}
         <span v-if="!isAffix(tag)" @click.prevent.stop="closeSelectedTag(tag)">
-          <close class="el-icon-close" style="width: 1em; height: 1em;vertical-align: middle;" />
+          <close
+            class="el-icon-close"
+            style="width: 1em; height: 1em; vertical-align: middle"
+          />
         </span>
       </router-link>
     </scroll-pane>
-    <ul v-show="visible" :style="{ left: left + 'px', top: top + 'px' }" class="contextmenu">
+    <ul
+      v-show="visible"
+      :style="{ left: left + 'px', top: top + 'px' }"
+      class="contextmenu"
+    >
       <li @click="refreshSelectedTag(selectedTag)">
-        <refresh-right style="width: 1em; height: 1em;" /> 刷新页面
+        <refresh-right class="menu-icon" />
+        <span class="menu-text">刷新页面</span>
       </li>
       <li v-if="!isAffix(selectedTag)" @click="closeSelectedTag(selectedTag)">
-        <close style="width: 1em; height: 1em;" /> 关闭当前
+        <close class="menu-icon" />
+        <span class="menu-text">关闭当前</span>
       </li>
       <li @click="closeOthersTags">
-        <circle-close style="width: 1em; height: 1em;" /> 关闭其他
+        <circle-close class="menu-icon" />
+        <span class="menu-text">关闭其他</span>
       </li>
       <li v-if="!isFirstView()" @click="closeLeftTags">
-        <back style="width: 1em; height: 1em;" /> 关闭左侧
+        <back class="menu-icon" />
+        <span class="menu-text">关闭左侧</span>
       </li>
       <li v-if="!isLastView()" @click="closeRightTags">
-        <right style="width: 1em; height: 1em;" /> 关闭右侧
+        <right class="menu-icon" />
+        <span class="menu-text">关闭右侧</span>
       </li>
       <li @click="closeAllTags(selectedTag)">
-        <circle-close style="width: 1em; height: 1em;" /> 全部关闭
+        <circle-close class="menu-icon" />
+        <span class="menu-text">全部关闭</span>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import ScrollPane from './ScrollPane'
-import { getNormalPath } from '@/utils/ruoyi'
-import useTagsViewStore from '@/store/modules/tagsView'
-import useSettingsStore from '@/store/modules/settings'
-import usePermissionStore from '@/store/modules/permission'
+import ScrollPane from "./ScrollPane";
+import { getNormalPath } from "@/utils/ruoyi";
+import useTagsViewStore from "@/store/modules/tagsView";
+import useSettingsStore from "@/store/modules/settings";
+import usePermissionStore from "@/store/modules/permission";
 
 const visible = ref(false);
 const top = ref(0);
@@ -64,75 +81,77 @@ const routes = computed(() => usePermissionStore().routes);
 const theme = computed(() => useSettingsStore().theme);
 
 watch(route, () => {
-  addTags()
-  moveToCurrentTag()
-})
+  addTags();
+  moveToCurrentTag();
+});
 
 watch(visible, (value) => {
   if (value) {
-    document.body.addEventListener('click', closeMenu)
+    document.body.addEventListener("click", closeMenu);
   } else {
-    document.body.removeEventListener('click', closeMenu)
+    document.body.removeEventListener("click", closeMenu);
   }
-})
+});
 
 onMounted(() => {
-  initTags()
-  addTags()
-})
+  initTags();
+  addTags();
+});
 
 function isActive(r) {
-  return r.path === route.path
+  return r.path === route.path;
 }
 
 function activeStyle(tag) {
-  if (!isActive(tag)) return {};
-  return {
-    "background-color": theme.value,
-    "border-color": theme.value
-  };
+  return {};
 }
 
 function isAffix(tag) {
-  return tag.meta && tag.meta.affix
+  return tag.meta && tag.meta.affix;
 }
 
 function isFirstView() {
   try {
-    return selectedTag.value.fullPath === '/index' || selectedTag.value.fullPath === visitedViews.value[1].fullPath
+    return (
+      selectedTag.value.fullPath === "/index" ||
+      selectedTag.value.fullPath === visitedViews.value[1].fullPath
+    );
   } catch (err) {
-    return false
+    return false;
   }
 }
 
 function isLastView() {
   try {
-    return selectedTag.value.fullPath === visitedViews.value[visitedViews.value.length - 1].fullPath
+    return (
+      selectedTag.value.fullPath ===
+      visitedViews.value[visitedViews.value.length - 1].fullPath
+    );
   } catch (err) {
-    return false
+    return false;
   }
 }
 
-function filterAffixTags(routes, basePath = '') {
-  let tags = []
-  routes.forEach(route => {
+function filterAffixTags(routes, basePath = "") {
+  let tags = [];
+  routes.forEach((route) => {
     if (route.meta && route.meta.affix) {
-      const tagPath = getNormalPath(basePath + '/' + route.path)
+      const tagPath = getNormalPath(basePath + "/" + route.path);
       tags.push({
         fullPath: tagPath,
         path: tagPath,
         name: route.name,
-        meta: { ...route.meta }
-      })
+        meta: { ...route.meta },
+      });
     }
     if (route.children) {
-      const tempTags = filterAffixTags(route.children, route.path)
+      const tempTags = filterAffixTags(route.children, route.path);
       if (tempTags.length >= 1) {
-        tags = [...tags, ...tempTags]
+        tags = [...tags, ...tempTags];
       }
     }
-  })
-  return tags
+  });
+  return tags;
 }
 
 function initTags() {
@@ -141,15 +160,15 @@ function initTags() {
   for (const tag of res) {
     // Must have tag name
     if (tag.name) {
-       useTagsViewStore().addVisitedView(tag)
+      useTagsViewStore().addVisitedView(tag);
     }
   }
 }
 
 function addTags() {
-  const { name } = route
+  const { name } = route;
   if (name) {
-    useTagsViewStore().addView(route)
+    useTagsViewStore().addView(route);
   }
 }
 
@@ -160,11 +179,11 @@ function moveToCurrentTag() {
         scrollPaneRef.value.moveToTarget(r);
         // when query is different then update
         if (r.fullPath !== route.fullPath) {
-          useTagsViewStore().updateVisitedView(route)
+          useTagsViewStore().updateVisitedView(route);
         }
       }
     }
-  })
+  });
 }
 
 function refreshSelectedTag(view) {
@@ -177,131 +196,210 @@ function refreshSelectedTag(view) {
 function closeSelectedTag(view) {
   proxy.$tab.closePage(view).then(({ visitedViews }) => {
     if (isActive(view)) {
-      toLastView(visitedViews, view)
+      toLastView(visitedViews, view);
     }
-  })
+  });
 }
 
 function closeRightTags() {
-  proxy.$tab.closeRightPage(selectedTag.value).then(visitedViews => {
-    if (!visitedViews.find(i => i.fullPath === route.fullPath)) {
-      toLastView(visitedViews)
+  proxy.$tab.closeRightPage(selectedTag.value).then((visitedViews) => {
+    if (!visitedViews.find((i) => i.fullPath === route.fullPath)) {
+      toLastView(visitedViews);
     }
-  })
+  });
 }
 
 function closeLeftTags() {
-  proxy.$tab.closeLeftPage(selectedTag.value).then(visitedViews => {
-    if (!visitedViews.find(i => i.fullPath === route.fullPath)) {
-      toLastView(visitedViews)
+  proxy.$tab.closeLeftPage(selectedTag.value).then((visitedViews) => {
+    if (!visitedViews.find((i) => i.fullPath === route.fullPath)) {
+      toLastView(visitedViews);
     }
-  })
+  });
 }
 
 function closeOthersTags() {
-  router.push(selectedTag.value).catch(() => { });
+  router.push(selectedTag.value).catch(() => {});
   proxy.$tab.closeOtherPage(selectedTag.value).then(() => {
-    moveToCurrentTag()
-  })
+    moveToCurrentTag();
+  });
 }
 
 function closeAllTags(view) {
   proxy.$tab.closeAllPage().then(({ visitedViews }) => {
-    if (affixTags.value.some(tag => tag.path === route.path)) {
-      return
+    if (affixTags.value.some((tag) => tag.path === route.path)) {
+      return;
     }
-    toLastView(visitedViews, view)
-  })
+    toLastView(visitedViews, view);
+  });
 }
 
 function toLastView(visitedViews, view) {
-  const latestView = visitedViews.slice(-1)[0]
+  const latestView = visitedViews.slice(-1)[0];
   if (latestView) {
-    router.push(latestView.fullPath)
+    router.push(latestView.fullPath);
   } else {
     // now the default is to redirect to the home page if there is no tags-view,
     // you can adjust it according to your needs.
-    if (view.name === 'Dashboard') {
+    if (view.name === "Dashboard") {
       // to reload home page
-      router.replace({ path: '/redirect' + view.fullPath })
+      router.replace({ path: "/redirect" + view.fullPath });
     } else {
-      router.push('/')
+      router.push("/");
     }
   }
 }
 
 function openMenu(tag, e) {
-  const menuMinWidth = 105
-  const offsetLeft = proxy.$el.getBoundingClientRect().left // container margin left
-  const offsetWidth = proxy.$el.offsetWidth // container width
-  const maxLeft = offsetWidth - menuMinWidth // left boundary
-  const l = e.clientX - offsetLeft + 15 // 15: margin right
+  const menuMinWidth = 120;
+  const menuHeight = 220; // 估算的菜单高度
+  const screenWidth = document.documentElement.clientWidth;
+  const screenHeight = document.documentElement.clientHeight;
 
-  if (l > maxLeft) {
-    left.value = maxLeft
+  // 获取鼠标位置
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  // 判断右侧空间是否足够
+  const rightSpace = screenWidth - mouseX;
+  const showOnRight = rightSpace >= menuMinWidth;
+
+  // 设置菜单位置
+  if (showOnRight) {
+    // 在鼠标右侧显示
+    left.value = mouseX + 5;
   } else {
-    left.value = l
+    // 在鼠标左侧显示
+    left.value = mouseX - menuMinWidth - 5;
   }
 
-  top.value = e.clientY
-  visible.value = true
-  selectedTag.value = tag
+  // 确保菜单不会超出屏幕底部
+  if (mouseY + menuHeight > screenHeight) {
+    top.value = screenHeight - menuHeight - 5;
+  } else {
+    top.value = mouseY;
+  }
+
+  visible.value = true;
+  selectedTag.value = tag;
 }
 
 function closeMenu() {
-  visible.value = false
+  visible.value = false;
 }
 
 function handleScroll() {
-  closeMenu()
+  closeMenu();
 }
 </script>
 
 <style lang="scss" scoped>
 .tags-view-container {
-  height: 34px;
+  height: 42px;
   width: 100%;
   background: var(--tags-bg, #fff);
-  border-bottom: 1px solid var(--tags-item-border, #d8dce5);
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .12), 0 0 3px 0 rgba(0, 0, 0, .04);
+  border-bottom: none;
+  box-shadow: 0 1px 20px rgba(0, 0, 0, 0.05);
+  padding: 0 12px;
 
   .tags-view-wrapper {
     .tags-view-item {
-      display: inline-block;
-      position: relative;
+      display: inline-flex;
+      align-items: center;
+      height: 30px;
+      line-height: 30px;
+      padding: 0 16px;
+      font-size: 13px;
+      margin: 6px 4px;
+      color: var(--tags-item-text, #666);
+      background: transparent;
+      border: none;
+      border-radius: 6px;
       cursor: pointer;
-      height: 26px;
-      line-height: 26px;
-      border: 1px solid var(--tags-item-border, #d8dce5);
-      color: var(--tags-item-text, #495060);
-      background: var(--tags-item-bg, #fff);
-      padding: 0 8px;
-      font-size: 12px;
-      margin-left: 5px;
-      margin-top: 4px;
+      user-select: none;
+      position: relative;
+      transition: all 0.25s cubic-bezier(0.3, 0, 0.2, 1);
 
-      &:first-of-type {
-        margin-left: 15px;
+      &::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        border-radius: 6px;
+        padding: 1px;
+        background: linear-gradient(90deg, #fe2c55, #f12b4e);
+        mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+        -webkit-mask: linear-gradient(#fff 0 0) content-box,
+          linear-gradient(#fff 0 0);
+        -webkit-mask-composite: xor;
+        mask-composite: exclude;
+        opacity: 0;
+        transition: opacity 0.25s;
       }
 
-      &:last-of-type {
-        margin-right: 15px;
+      &:hover {
+        color: #fe2c55;
+        background: rgba(254, 44, 85, 0.04);
+
+        &::before {
+          opacity: 0.5;
+        }
+
+        .el-icon-close {
+          opacity: 1;
+          transform: scale(1);
+        }
       }
 
       &.active {
-        background-color: #42b983;
         color: #fff;
-        border-color: #42b983;
+        background: linear-gradient(90deg, #fe2c55, #f12b4e);
+        border-color: transparent;
+        font-weight: 500;
+        transform: translateY(-1px);
+        box-shadow: 0 4px 16px rgba(254, 44, 85, 0.25);
 
-        &::before {
-          content: '';
-          background: #fff;
-          display: inline-block;
-          width: 8px;
-          height: 8px;
-          border-radius: 50%;
-          position: relative;
-          margin-right: 5px;
+        &:focus,
+        &:hover {
+          color: #fff;
+          background: linear-gradient(90deg, #fe2c55, #f12b4e);
+          border-color: transparent;
+        }
+      }
+
+      .el-icon-close {
+        width: 16px;
+        height: 16px;
+        margin-left: 6px;
+        border-radius: 50%;
+        color: #999;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        opacity: 0;
+        transition: all 0.25s cubic-bezier(0.3, 0, 0.2, 1);
+        vertical-align: middle;
+        transform-origin: center;
+
+        &:hover {
+          background: rgba(254, 44, 85, 0.1);
+          color: #fe2c55;
+          transform: scale(1.2);
+        }
+      }
+
+      &:hover {
+        .el-icon-close {
+          opacity: 1;
+        }
+      }
+
+      &.active {
+        .el-icon-close {
+          color: rgba(255, 255, 255, 0.9);
+
+          &:hover {
+            background: rgba(0, 0, 0, 0.2);
+            color: #fff;
+          }
         }
       }
     }
@@ -309,25 +407,141 @@ function handleScroll() {
 
   .contextmenu {
     margin: 0;
-    background: var(--el-bg-color-overlay, #fff);
+    background: rgba(255, 255, 255, 0.98);
+    border: none;
+    border-radius: 12px;
+    padding: 6px;
+    min-width: 180px;
+    box-shadow: 0 4px 32px rgba(0, 0, 0, 0.1), 0 2px 16px rgba(0, 0, 0, 0.05);
     z-index: 3000;
-    position: absolute;
-    list-style-type: none;
-    padding: 5px 0;
-    border-radius: 4px;
-    font-size: 12px;
-    font-weight: 400;
-    color: var(--tags-item-text, #333);
-    box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, .3);
-    border: 1px solid var(--el-border-color-light, #e4e7ed);
+    position: fixed;
+    list-style: none;
+    backdrop-filter: blur(20px);
+    transform-origin: top left;
+    animation: menuFadeIn 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+
+    @keyframes menuFadeIn {
+      from {
+        opacity: 0;
+        transform: scale(0.9);
+      }
+      to {
+        opacity: 1;
+        transform: scale(1);
+      }
+    }
 
     li {
-      margin: 0;
-      padding: 7px 16px;
+      display: flex;
+      align-items: center;
+      padding: 10px 16px;
+      margin: 2px;
+      font-size: 14px;
+      color: #333;
+      border-radius: 8px;
       cursor: pointer;
+      transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+      position: relative;
+      overflow: hidden;
+      height: 42px;
+
+      &::before {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(90deg, #fe2c55, #f12b4e);
+        opacity: 0;
+        transition: opacity 0.2s;
+      }
+
+      .menu-icon {
+        width: 18px;
+        height: 18px;
+        margin-right: 20px;
+        position: relative;
+        z-index: 1;
+        transition: transform 0.2s;
+        flex-shrink: 0;
+      }
+
+      .menu-text {
+        position: relative;
+        z-index: 1;
+        flex-grow: 1;
+        white-space: nowrap;
+      }
 
       &:hover {
-        background: var(--tags-item-hover, #eee);
+        color: #fff;
+        transform: translateY(-1px);
+
+        &::before {
+          opacity: 1;
+        }
+
+        .menu-icon {
+          transform: scale(1.1);
+          filter: brightness(1.2);
+        }
+      }
+
+      &:active {
+        transform: scale(0.96);
+      }
+    }
+  }
+}
+
+// 暗黑模式适配
+html.dark {
+  .tags-view-container {
+    background: #141414;
+
+    .tags-view-item {
+      color: #999;
+
+      &:hover {
+        color: #ff4d4d;
+        background: rgba(255, 77, 77, 0.1);
+      }
+
+      &.active {
+        background: linear-gradient(90deg, #fe2c55, #f12b4e);
+        color: #fff;
+      }
+
+      .el-icon-close:hover {
+        background: rgba(255, 77, 77, 0.15);
+        color: #ff4d4d;
+      }
+    }
+
+    .contextmenu {
+      background: rgba(29, 29, 29, 0.98);
+      box-shadow: 0 4px 32px rgba(0, 0, 0, 0.2), 0 2px 16px rgba(0, 0, 0, 0.1);
+      border: 1px solid rgba(255, 255, 255, 0.08);
+
+      li {
+        color: #e5e5e5;
+
+        .menu-icon {
+          opacity: 0.9;
+        }
+
+        &:hover {
+          background: none;
+
+          &::before {
+            opacity: 1;
+          }
+
+          .menu-icon {
+            opacity: 1;
+          }
+        }
       }
     }
   }
@@ -339,25 +553,14 @@ function handleScroll() {
 .tags-view-wrapper {
   .tags-view-item {
     .el-icon-close {
-      width: 16px;
-      height: 16px;
-      vertical-align: 2px;
+      width: 16px !important;
+      height: 16px !important;
       border-radius: 50%;
       text-align: center;
-      transition: all .3s cubic-bezier(.645, .045, .355, 1);
-      transform-origin: 100% 50%;
 
       &:before {
-        transform: scale(.6);
+        transform: scale(0.6);
         display: inline-block;
-        vertical-align: -3px;
-      }
-
-      &:hover {
-        background-color: var(--tags-close-hover, #b4bccc);
-        color: #fff;
-        width: 12px !important;
-        height: 12px !important;
       }
     }
   }
